5cd03620cc0c2857268a01485f00d34a1ea54801
Leo Lundberg Sehlin <leolun02@elevad.umea.se>
2024-03-11 11:41:37 +0100

    klar med register

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    function handleSubmit() {
+    // Perform form submission logic here
+    console.log('Form submitted!');
+    }
+</script>
+
+<h1>Welcome back!</h1>
+
+<form    on:submit|preventDefault={handleSubmit}>
+    <label for = "email">Email:</label>
+    <input id="email"
+    type="text"/>
+    <label for = "password">Password:</label>
+    <input id="password"
+    type="text"/>
+    <button type="submit">Log in</button>
+</form>
+
+<style>
+form{
+    display:grid;
+    width: 60vw;
+    max-width:500px;
+    min-width:300px;
+    margin:auto;
+    grid-template-columns: repeat(2, minmax(200px, 1fr));
+}
+</style>
+
+<p>Don't have an account? <a
+
+    href="/register">Register</a>></p>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+    function handleSubmit() {
+    // Perform form submission logic here
+    console.log('Form submitted!');
+    }
+
+    let firstName = '';
+    let lastName = '';
+</script>
+<h1>Hello {firstName+' '+lastName}!</h1>
+
+<h1>Registration Form</h1>
+
+<form    on:submit|preventDefault={handleSubmit}>
+    <label for = "firstName">First Name:</label>
+    <input  bind:value={firstName} id="firstName"
+    type="text"/>
+    <label for = "lastName">Last Name:</label>
+    <input bind:value ={lastName} id="lastName"
+    type="text"/>
+    <label for = "email">Email:</label>
+    <input id="email"
+    type="text"/>
+    <label for = "password">Password:</label>
+    <input id="password"
+    type="text"/>
+    <button type="submit">Register</button>
+</form>
+
+<style>
+form{
+    display:grid;
+    width: 60vw;
+    max-width:500px;
+    min-width:300px;
+    margin:auto;
+    grid-template-columns: repeat(2, minmax(200px, 1fr));
+}
+</style>
+
+<p>Have an account? <a
+
+    href="/login">Log in</a>></p>
